25a420939e6953bcb9ce9545c8333be69754ef95
Harshcreator <jayson22603@gmail.com>
2025-02-24 19:15:22 +0530

    refactor: remove blog entries from ArrowCard and RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,13 +7,11 @@ type Context = {
 }
 
 export async function GET(context: Context) {
-  const blog = (await getCollection("blog"))
-  .filter(post => !post.data.draft);
 
   const projects = (await getCollection("projects"))
     .filter(project => !project.data.draft);
 
-  const items = [...blog, ...projects]
+  const items = [...projects]
     .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
 
   return rss({
